5ba9e9d0571563bd06b97f6ac8ac2af27fb328d8
Marvin Rocha <marvinvca@outlook.com>
2025-06-12 00:01:10 -0300

    Removes unused reducer logic and debug UI
    Simplifies the context provider by eliminating the reducer and related UI elements.
    Children rendering now drives component usage.

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import type { TaskStateModel } from '../../models/TaskStateModel';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
@@ -10,58 +10,9 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = React.useState<TaskStateModel>(initialTaskState);
 
-  const [numero, dispatch] = useReducer((state, action) => {
-    console.log('action:', action);
-    console.log('state', state);
-
-    switch (action) {
-      case 'Increment':
-        return state + 1;
-      case 'Decrement':
-        return state - 1;
-      case 'Double':
-        return state * 2;
-      case 'Reset':
-        return state - state;
-      default:
-        return state;
-    }
-
-    return state;
-  }, 0);
-
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {/* {children} */}
-      <h1>O numero é: {numero}</h1>
-      <button
-        onClick={() => {
-          dispatch('Increment');
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch('Decrement');
-        }}
-      >
-        Decrement
-      </button>
-      <button
-        onClick={() => {
-          dispatch('Double');
-        }}
-      >
-        Double
-      </button>
-      <button
-        onClick={() => {
-          dispatch('Reset');
-        }}
-      >
-        Reset
-      </button>
+      {children}
     </TaskContext.Provider>
   );
 }
